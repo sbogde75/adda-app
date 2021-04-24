fd60fa5a83ab0477656d508375f2949760acf156
Sorin <Sorin.Bogde@Chubb.com>
2021-04-24 12:54:36 +0100

    test cu Adda

---
+++ src/components/Like.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+// import RestoreIcon from '@material-ui/icons/Restore';
+// import FavoriteIcon from '@material-ui/icons/Favorite';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+// import LocationOnIcon from '@material-ui/icons/LocationOn';
+// import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+// import TrendingFlatIcon from '@material-ui/icons/TrendingFlat';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+
+const useStyles = makeStyles({
+  root: {
+    width: 500,
+  },
+});
+
+export default function Like() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(false);
+
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.root}
+    >
+      <BottomNavigationAction
+        label='Favorites'
+        style={{ display: 'none' }}
+        icon={<FavoriteBorderIcon />}
+      />
+      <BottomNavigationAction label='Favorites' icon={<FavoriteBorderIcon />} />
+      <BottomNavigationAction label='Details' icon={<ArrowForwardIosIcon />} />
+      {/* <BottomNavigationAction label={value} icon={<LocationOnIcon />} /> */}
+    </BottomNavigation>
+  );
+}
--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+import Like from './Like';
 import Typography from '@material-ui/core/Typography';
-
 import useStyles from '../css/useStyles';
 
 function MyCard() {
@@ -31,12 +31,13 @@ function MyCard() {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size='small' color='primary'>
+        <Like />
+        {/* <Button size='small' color='primary'>
           View
         </Button>
         <Button size='small' color='primary'>
           Edit
-        </Button>
+        </Button> */}
       </CardActions>
     </Card>
   );
